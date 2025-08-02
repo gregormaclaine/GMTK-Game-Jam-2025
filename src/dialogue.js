4897a154e926e27bac7e312e5d185ec26bb58974
Gregor Maclaine <thegregormaclaine@gmail.com>
2025-08-02 19:05:20 +0100

    fix

--- src/dialogue.js
+++ src/dialogue.js
@@ -5,7 +5,7 @@ const DIALOGUE = {
       text: 'Hello there, my name is Cara. Welcome to our humble planet :3'
     },
     {
-      profile: 'ranger',
+      profile: 'ranger_f',
       text: "Hello Cara! I'm cat, it's a pleasure to meet you."
     },
     {
@@ -13,7 +13,7 @@ const DIALOGUE = {
       text: 'Cat? Your name is just cat?'
     },
     {
-      profile: 'ranger',
+      profile: 'ranger_f',
       text: "cat. i've been stripped down to a shadow because of what happened on our planet. This is who i am now."
     }
   ]
